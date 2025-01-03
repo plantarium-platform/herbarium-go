bd23b4057b635f9ff2aa52e7d066d38ebde18923
Aleksandr Kichev <kichev.alexander@gmail.com>
2025-01-03 16:27:09 +0800

    Attempt to fix logging for integration tests

--- cmd/herbarium/main.go
+++ cmd/herbarium/main.go
@@ -5,9 +5,23 @@ import (
 	"log"
 
 	"github.com/plantarium-platform/herbarium-go/internal/manager"
+	"go.uber.org/zap"
 )
 
 func main() {
+	// Initialize the Zap logger
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Fatalf("Failed to initialize Zap logger: %v", err)
+	}
+	defer logger.Sync()
+
+	// Replace the standard logger with Zap
+	zap.ReplaceGlobals(logger)
+	if err := zap.RedirectStdLog(logger); err != nil {
+		log.Fatalf("Failed to redirect standard log to Zap: %v", err)
+	}
+
 	// Create a new PlatformManager instance with dependencies initialized internally
 	platformManager, err := manager.NewPlatformManagerWithDI()
 	if err != nil {
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/go-resty/resty/v2 v2.16.0
 	github.com/jarcoal/httpmock v1.3.1
 	github.com/stretchr/testify v1.9.0
+	go.uber.org/zap v1.27.0
 	gopkg.in/yaml.v2 v2.4.0
 )
 
@@ -14,6 +15,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/net v0.27.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
